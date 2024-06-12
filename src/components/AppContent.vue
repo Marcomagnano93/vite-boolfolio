cc131e033ca4c52b966c6f07fede58650eb30cf7
Marcomagnano93 <marcog.magnano@gmail.com>
2024-06-12 16:12:00 +0200

    ADD: component project card (mileston 5)

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,31 +1,11 @@
 <script>
-import axios from 'axios'
+import ProjectCard from './ProjectCard.vue';
 
 export default {
-  data() {
-    return {
-      projects: []
-    }
-  },
-  methods: {
-    fetchData() {
-
-      axios.get('http://127.0.0.1:8000/api/projects',{
-        params: {
-
-        }
-      })
-      .then((res) => {
-        console.log(res.data.projects)
-        this.projects = res.data.projects
-      })
-    }
-  },
-  created() {
-    this.fetchData()
-  },
+  components: {
+    ProjectCard
+  }
 }
-
 </script>
 
 <template>
@@ -34,22 +14,9 @@ export default {
     <section class="h-100">
         <div class="container">
           <h2>Progetti:</h2>
-          <ul class="row gap-3">
-            <li class="card" v-for="project in projects">
-                <div class="card-header">
-                    {{ project.title }}
-                </div>
-                <div class="card-body">
-                  <p>
-                    {{ project.description }}
-                  </p>
-                  <p>
-                    <a :href="project.git">Git Link</a>
-                  </p>
-                    
-                </div>
-            </li> 
-          </ul>
+          
+           <ProjectCard></ProjectCard>
+           
         </div>
     </section>
   </main>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,51 @@
+<script>
+import axios from 'axios';
+
+
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  methods: {
+    fetchData() {
+
+      axios.get('http://127.0.0.1:8000/api/projects',{
+        params: {
+
+        }
+      })
+      .then((res) => {
+        console.log(res.data.projects)
+        this.projects = res.data.projects
+      })
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+}
+
+</script>
+
+<template>
+        <ul class="row gap-3">
+            <li class="card" v-for="project in projects">
+                <div class="card-header">
+                    {{ project.title }}
+                </div>
+                <div class="card-body">
+                  <p>
+                    {{ project.description }}
+                  </p>
+                  <p>
+                    <a :href="project.git">Git Link</a>
+                  </p>
+                    
+                </div>
+            </li>
+        </ul>
+</template>
+
+<style></style>
