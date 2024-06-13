106c386cd0c6d21e3e9e0b7e228b96c01761e54e
Marcomagnano93 <marcog.magnano@gmail.com>
2024-06-13 14:56:49 +0200

    ADD: router, import pages in main.js, create pages & .vue page files

--- src/App.vue
+++ src/App.vue
@@ -20,14 +20,17 @@ export default {
 </script>
 
 <template>
-
-  <div id="app">
-    <AppHeader></AppHeader>
-    <AppContent></AppContent>
-    <AppFooter></AppFooter>
-
-  </div>
-
+  <h1>Hello App!</h1>
+  <p>
+    <strong>Current route path:</strong> {{ $route.fullPath }}
+  </p>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -8,7 +8,20 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+// import pages route
+import { createRouter, createWebHistory } from 'vue-router'
+import AppHome from './pages/AppHome.vue'
+import AppAbout from './pages/AppPortfolio.vue'
+import AppBlog from './pages/AppContact.vue'
 
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: '/', name: 'home', component: AppHome },
+		{ path: '/portfolio', name: 'portfolio', component: AppPortfolio },
+		{ path: '/contact', name: 'contact', component: AppContact },
+	],
+})
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -20,4 +33,4 @@ const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 
-app.mount('#app')
+app.use(router).mount('#app')
