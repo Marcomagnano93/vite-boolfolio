3ceb89e273eea3da55ae58289d6856982495dac3
Marcomagnano93 <marcog.magnano@gmail.com>
2024-06-13 16:00:48 +0200

    refact: contact

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -7,18 +7,24 @@
     <div class="container">
         <h1>Contattaci</h1>
 
-        <div class="mb-3">
-            <label for="name" class="form-label">Nome:</label>
-            <input type="text" class="form-control" id="name" placeholder="il tuo nome">
-        </div>
-        <div class="mb-3">
-            <label for="mail" class="form-label">La tua mail:</label>
-            <input type="email" class="form-control" id="mail" placeholder="mail@esempio.com">
-        </div>
-        <div class="mb-3">
-            <label for="message" class="form-label">Il tuo messaggio:</label>
-            <textarea class="form-control" id="message" rows="3"></textarea>
-        </div>
+        <form action="">
+            <div class="mb-3">
+                <label for="name" class="form-label">Nome:</label>
+                <input type="text" class="form-control" id="name" placeholder="il tuo nome">
+            </div>
+            <div class="mb-3">
+                <label for="mail" class="form-label">La tua mail:</label>
+                <input type="email" class="form-control" id="mail" placeholder="mail@esempio.com">
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Il tuo messaggio:</label>
+                <textarea class="form-control" id="message" rows="3"></textarea>
+            </div>
+            <div class="mb-3">
+                <input class="btn btn-primary" type="submit" value="Invia">
+            </div>
+        </form>
+
     </div>
 
 </template>
