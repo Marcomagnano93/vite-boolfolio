96eba63c99faee8722883280b58c068981a5e5fe
Marcomagnano93 <marcog.magnano@gmail.com>
2024-06-13 15:36:39 +0200

    ADD: app contact

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -4,6 +4,23 @@
 
 <template>
 
+    <div class="container">
+        <h1>Contattaci</h1>
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Nome:</label>
+            <input type="text" class="form-control" id="name" placeholder="il tuo nome">
+        </div>
+        <div class="mb-3">
+            <label for="mail" class="form-label">La tua mail:</label>
+            <input type="email" class="form-control" id="mail" placeholder="mail@esempio.com">
+        </div>
+        <div class="mb-3">
+            <label for="message" class="form-label">Il tuo messaggio:</label>
+            <textarea class="form-control" id="message" rows="3"></textarea>
+        </div>
+    </div>
+
 </template>
 
 <style></style>
